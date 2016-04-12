88cedd01bc724fcfef767e09b20615d29cf9069c
Alexander Netrusov <Alexander.Netrusov@gmail.com>
2016-04-12 13:23:50 +0300

    now parsing multiline and one line logfiles

--- logparse.pl
+++ logparse.pl
@@ -11,8 +11,9 @@ use FindBin               ();
 use Digest::MD5           ();
 use File::Spec::Functions ();
 
-Carp::confess "Usage: ${PROGRAM_NAME} <file> <mask> <match> [<except>]"
-    if !@ARGV || scalar @ARGV < 3;
+Carp::confess
+  "Usage: ${PROGRAM_NAME} <file>[+multiline] <mask> <match> [<except>]"
+  if !@ARGV || scalar @ARGV < 3;
 
 my $argv = {};
 
@@ -20,40 +21,40 @@ for (qw|file mask match except|) {
     $argv->{$_} = shift @ARGV;
 }
 
-open my $fh, '<', $argv->{file}
-    or Carp::confess sprintf "Failed to open '%s': %s", $argv->{file},
-    $EXTENDED_OS_ERROR;
+my ( $file, $multiline ) = $argv->{file} =~ m/(.+?)([+]multi[line]{0,4})?$/xms;
 
-seek $fh, _readpos( $argv->{file} ), 0;
+open my $fh, '<', $file
+  or Carp::confess sprintf "Failed to open '%s': %s", $file,
+  $EXTENDED_OS_ERROR;
 
-my ( $entry, $pos );
+seek $fh, readpos($file), 0;
+
+my $entry = {};
 
 while (<$fh>) {
-    if ( m/($argv->{mask})/xms || eof $fh ) {
-        $entry .= $_ if eof $fh;
-
-        if ($entry) {
-            if ( $entry =~ m/$argv->{match}/xms ) {
-                next
-                    if $argv->{except}
-                    && $entry =~ m/$argv->{except}/xms;
-
-                print $entry, $/;
-            }
-        }
-
-        $entry = $_;
-        next;
+    if ( m/$argv->{mask}/xms || eof $fh ) {
+        $entry->{final} = delete $entry->{temp};
+        $entry->{temp}  = $_;
     }
-    $entry .= $_;
+    else {
+        $entry->{temp} .= $_ if $multiline;
+    }
+
+    if ( $entry->{final} && $entry->{final} =~ m/$argv->{match}/xms ) {
+        next
+          if $argv->{except}
+          && $entry->{final} =~ m/$argv->{except}/xms;
+
+        print delete $entry->{final}, $/;
+    }
 }
 
-_writepos( $argv->{file}, tell $fh );
+writepos( $file, tell $fh );
 
 close $fh
-    or Carp::confess 'Failed to close filehandle: ' . $EXTENDED_OS_ERROR;
+  or Carp::confess 'Failed to close filehandle: ' . $EXTENDED_OS_ERROR;
 
-sub _readpos {
+sub readpos {
     my ($file) = @_;
 
     $file = File::Spec::Functions::catfile( $FindBin::Bin,
@@ -63,34 +64,35 @@ sub _readpos {
 
     if ( -f $file ) {
         open my $fh, '<', $file
-            or Carp::confess "Failed to open '$file': " . $EXTENDED_OS_ERROR;
+          or Carp::confess "Failed to open '$file': " . $EXTENDED_OS_ERROR;
 
         chomp( $pos = <$fh> );
 
         close $fh
-            or Carp::confess 'Failed to close filehandle: '
-            . $EXTENDED_OS_ERROR;
+          or Carp::confess 'Failed to close filehandle: ' . $EXTENDED_OS_ERROR;
     }
 
     return $pos;
 }
 
-sub _writepos {
+sub writepos {
     my ( $file, $pos ) = @_;
 
     $file = File::Spec::Functions::catfile( $FindBin::Bin,
         q{.} . Digest::MD5::md5_hex($file) );
 
     open my $fh, '>', $file
-        or Carp::confess sprintf "Failed to open '%s': %s", $file,
-        $EXTENDED_OS_ERROR;
+      or Carp::confess sprintf "Failed to open '%s': %s", $file,
+      $EXTENDED_OS_ERROR;
 
     print {$fh} $pos
-        or Carp::confess sprintf "Failed to write to '%s': %s", $file,
-        $EXTENDED_OS_ERROR;
+      or Carp::confess sprintf "Failed to write to '%s': %s", $file,
+      $EXTENDED_OS_ERROR;
 
     close $fh
-        or Carp::confess 'Failed to close filehandle: ' . $EXTENDED_OS_ERROR;
+      or Carp::confess 'Failed to close filehandle: ' . $EXTENDED_OS_ERROR;
 
     return 1;
 }
+
+__END__
